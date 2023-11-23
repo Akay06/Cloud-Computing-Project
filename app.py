fdb7c3bf394e43d3c685584614881fee46689fb4
Akay06 <72030643+Akay06@users.noreply.github.com>
2023-11-23 06:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from flask import Flask, render_template, request
-from nltk.tokenize import word_tokenize, sent_tokenize
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/process', methods=['POST'])
-def process_text():
-    text = request.form['input_text']
-
-    # Example: Tokenize the text
-    words = word_tokenize(text)
-    sentences = sent_tokenize(text)
-
-    return render_template('result.html', words=words, sentences=sentences)
 
 if __name__ == '__main__':
     app.run(debug=True)
